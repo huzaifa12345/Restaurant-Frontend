5fb5052a1c34428657465c27bd0760dfc5004ad1
huzaifa12345 <huzaifa.suleman12345@gmail.com>
2026-07-18 00:11:37 +0500

    url added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://YOUR_RENDER_API.onrender.com/api',
-  mediaBaseUrl: 'https://YOUR_RENDER_API.onrender.com'
+  apiUrl: 'https://restaurant-backend-l4wu.onrender.com/api',
+  mediaBaseUrl: 'https://restaurant-backend-l4wu.onrender.com'
 };
